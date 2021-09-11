4fb4d6b68159f2ba77e4c7f8a21248a290c1a3a9
Batin EVIRGEN <batinevirgen@gmail.com>
2021-09-11 22:40:54 +0300

    Added Gallery component for listing characters.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
 import Sidebar from "../components/Sidebar"
 import MainSearch from "../components/MainSearch"
+import Gallery from "../components/Gallery"
+
+import sampleImage from "../public/placeholder.png";
 
 export default function Home() {
+  const items = [
+    { name: "Isaac", url: "#", image: sampleImage },
+    { name: "Magdalene", url: "#", image: sampleImage },
+    { name: "Cain", url: "#", image: sampleImage },
+    { name: "Judas", url: "#", image: sampleImage },
+    { name: "???", url: "#", image: sampleImage },
+    { name: "Eve", url: "#", image: sampleImage },
+    { name: "Samson", url: "#", image: sampleImage },
+    { name: "Azazel", url: "#", image: sampleImage },
+    { name: "Lazarus", url: "#", image: sampleImage },
+    { name: "Eden", url: "#", image: sampleImage },
+    { name: "The Lost", url: "#", image: sampleImage },
+    { name: "Lilith", url: "#", image: sampleImage },
+    { name: "Keeper", url: "#", image: sampleImage },
+    { name: "Apollyon", url: "#", image: sampleImage },
+    { name: "The Forgotten", url: "#", image: sampleImage },
+    { name: "Bethany", url: "#", image: sampleImage },
+    { name: "Jacob and Esau", url: "#", image: sampleImage },
+  ]
+
   return (
     <div>
       <Sidebar>
@@ -9,8 +32,9 @@ export default function Home() {
       </Sidebar>
       <div className="ml-64">
         <MainSearch placeholder="Search anything"/>
-      
-        Main content
+        <div className="p-8">
+          <Gallery title="Characters" items={ items } />
+        </div>
       </div>
     </div>
   )
