95a4cbaf90e9220e23ecb1b7747ad4430b889e42
Batin EVIRGEN <batinevirgen@gmail.com>
2021-09-13 21:42:22 +0300

    Added start conditions to CharacterStats component.

--- components/CharacterStats/CharacterStats.js
+++ components/CharacterStats/CharacterStats.js
@@ -1,36 +1,60 @@
+import { generateHeartImages } from "../../lib/image";
+
 export default function CharacterStats({ character, imageScale }) {
+  const heartScale = 4/6;
+  const heartImages = generateHeartImages(character.Heart.RedHeart, character.Heart.SoulHeart, character.Heart.BlackHeart, character.Heart.CoinHeart, character.Heart.BoneHeart, heartScale);
+  
   return (
     <div>
       <img src={ `data:${ character.Image.mime };base64,${ character.Image.base64 }` } alt={ character.Image.alternativeText } width={ character.Image.width * imageScale } height={ character.Image.height * imageScale } className="mx-auto pb-4" />
       <table className="p-16 mx-auto w-full">
         <thead>
-          <tr className="bg-gray-200">
+            <th className="font-medium text-center bg-gray-900 text-gray-200" colSpan="2">
+              Starting Items
+            </th>
+        </thead>
+        <tbody>
+          <tr className="bg-gray-100">
+            <td className="px-4 py-2 font-medium">Heart</td>
+            <td className="px-4 py-2">{ heartImages }</td>
+          </tr>
+          <tr>
+            <td className="px-4 py-2 font-medium">Bomb</td>
+            <td className="px-4 py-2">1</td>
+          </tr>
+          <tr className="bg-gray-100">
+            <td className="px-4 py-2 font-medium">Key</td>
+            <td className="px-4 py-2">0</td>
+          </tr>
+        </tbody>
+        <thead>
+          <tr className="bg-gray-900 text-gray-200">
             <th>Stat</th>
             <th>Value</th>
           </tr>
         </thead>
         <tbody>
-          <tr >
+          <tr className="bg-gray-100">
             <td className="px-4 py-2 font-medium">Damage</td>
             <td className="px-4 py-2">{ character.Damage }</td>
           </tr>
-          <tr className="bg-gray-200">
+          <tr>
             <td className="px-4 py-2 font-medium">Tears</td>
             <td className="px-4 py-2">{ character.Tears }</td>
           </tr>
-          <tr>
+          <tr className="bg-gray-100">
             <td className="px-4 py-2 font-medium">Shot Speed</td>
             <td className="px-4 py-2">{ character.ShotSpeed }</td>
           </tr>
-          <tr className="bg-gray-200">
+          <tr>
             <td className="px-4 py-2 font-medium">Range</td>
             <td className="px-4 py-2">{ character.Range }</td>
           </tr>
-          <tr>
+          <tr className="bg-gray-100">
             <td className="px-4 py-2 font-medium">Speed</td>
             <td className="px-4 py-2">{ character.Speed }</td>
           </tr>
-          <tr className="bg-gray-200">
+          <tr>
             <td className="px-4 py-2 font-medium">Luck</td>
             <td className="px-4 py-2">{ character.Luck }</td>
           </tr>
